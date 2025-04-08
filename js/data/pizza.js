557133140f293e06af7e24a83060e2422b46f45a
Mantas Kukulskis <8mantas8@gmail.com>
2025-04-08 14:44:01 +0300

    import

---
+++ js/data/pizza.js
@@ -0,0 +1,38 @@
+export const foodList = [
+    {
+        photo: 'pizza.png',
+        name: 'Margarita',
+        price: 449,
+        availability: 4,
+    },
+    {
+        photo: 'pizza.png',
+        name: 'Havaju',
+        price: 899,
+        availability: 5,
+    },
+    {
+        photo: 'pizza.png',
+        name: '4 sezonu',
+        price: 1299,
+        availability: 3,
+    },
+    {
+        photo: 'pizza.png',
+        name: 'Ham & Mushrooms',
+        price: 2200,
+        availability: 7,
+    },
+    {
+        photo: 'pizza.png',
+        name: '4 suriu',
+        price: 1313,
+        availability: 8,
+    },
+    {
+        photo: 'pizza.png',
+        name: 'Mafia',
+        price: 1699,
+        availability: 0,
+    }
+];
