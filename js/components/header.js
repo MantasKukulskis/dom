a53d4ff0a51dd4563964b615e45621172054150e
Mantas Kukulskis <8mantas8@gmail.com>
2025-04-09 17:30:23 +0300

    up

--- js/components/header.js
+++ js/components/header.js
@@ -1,29 +1,29 @@
 export function header() {
     const menu = [
-        { text: 'Home', href: '/' },
-        { text: 'Text', href: '/text' },
-        { text: 'Food', href: '/food' },
-        { text: 'Darzas', href: '/darzas' },
-        { text: 'Header', href: '/header' },
-        { text: 'Click', href: '/click' },
+        { text: 'Home', href: '' },
+        { text: 'Text', href: 'text' },
+        { text: 'Food', href: 'food' },
+        { text: 'Darzas', href: 'darzas' },
+        { text: 'Header', href: 'header' },
+        { text: 'Click', href: 'click' },
     ];
 
-    const lp = location.pathname;
-    const currentPage = lp.length > 1 && lp.at(-1) === '/' ? lp.slice(0, -1) : lp;
+    const basePath = window.location.hostname === 'localhost'
+        ? '/'
+        : '/dom/';
+
+    const currentPath = window.location.pathname;
     let linksHTML = '';
 
     for (const link of menu) {
-        let activePage = '';
-        if (link.href === currentPage) {
-            activePage = 'active';
-        }
-
-        linksHTML += `<a class="link ${activePage}" href="${link.href}">${link.text}</a>`;
+        const fullHref = `${basePath}${link.href}`;
+        const active = currentPath === fullHref ? 'active' : '';
+        linksHTML += `<a class="link ${active}" href="${fullHref}">${link.text}</a>`;
     }
 
     const HTML = `
         <header class="main-header">
-            <img class="logo" src="../food/pizza.png" alt="Logo">
+            <img class="logo" src="${basePath}food/pizza.png" alt="Logo">
             <nav class="main-nav">${linksHTML}</nav>
         </header>`;
 
